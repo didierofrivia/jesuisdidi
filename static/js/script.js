27d200e31f4792cf047f0a7a73f161de7e414ba7
dd di cesare <didi@posteo.net>
2025-08-28 19:42:29 +0200

    [js] New script for creating independent "type writers"
    Signed-off-by: dd di cesare <didi@posteo.net>

--- static/js/script.js
+++ static/js/script.js
@@ -1,39 +1,44 @@
-// typewritter
-let typeJsText = document.querySelector(".animatedText");
-let stringIndex = 0;
-let charIndex = 0;
-let isTyping = true;
+function createTypewriter(textArray, selector) {
+    let stringIndex = 0;
+    let charIndex = 0;
+    let isTyping = true;
+    let typeJsText = document.querySelector(selector);
 
-function typeJs() {
-    if (stringIndex < textArray.length) {
-        const currentString = textArray[stringIndex];
-
-        if (isTyping) {
-            if (charIndex < currentString.length) {
-                typeJsText.innerHTML += currentString.charAt(charIndex);
-                charIndex++;
-            } else {
-                isTyping = false;
-            }
-        } else {
-            if (charIndex > 0) {
-                charIndex = 0;
-                typeJsText.innerHTML = "";
-                //typeJsText.innerHTML = currentString.substring(0, charIndex - 1);
-                //charIndex--;
-            } else {
-                isTyping = true;
-                stringIndex++;
-
-                if (stringIndex >= textArray.length) {
-                    stringIndex = 0;
+    return {
+        write: function() {
+            if (stringIndex < textArray.length) {
+                const currentString = textArray[stringIndex]
+                if (isTyping) {
+                if (charIndex < currentString.length) {
+                    typeJsText.innerHTML += currentString.charAt(charIndex);
+                    charIndex++;
+                } else {
+                    isTyping = false;
                 }
+                } else {
+                    if (charIndex > 0) {
+                        charIndex = 0;
+                        typeJsText.innerHTML = "";
+                        //typeJsText.innerHTML = currentString.substring(0, charIndex - 1);
+                        //charIndex--;
+                    } else {
+                        isTyping = true;
+                        stringIndex++;
 
-                charIndex = 0;
-                typeJsText.innerHTML = "";
+                        if (stringIndex >= textArray.length) {
+                            stringIndex = 0;
+                        }
+
+                        charIndex = 0;
+                        typeJsText.innerHTML = "";
+                    }
+                }
             }
         }
     }
 }
 
-setInterval(typeJs, 120);
+function typeJs(textArray, selector) {
+    const typeWriter = createTypewriter(textArray, selector)
+    setInterval(typeWriter.write, 120)
+}
